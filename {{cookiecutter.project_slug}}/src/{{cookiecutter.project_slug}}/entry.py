f096abb6cc1b3fe2672ff3ec0bb6f4c9036e6e9c
Sameh Mohamed <sameh.mohamed@mastercard.com>
2021-12-24 02:06:07 +0000

    simplify

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/entry.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/entry.py
@@ -3,7 +3,8 @@ import {{cookiecutter.project_slug}}
 
 def main():
     """Return the main entry point for the program."""
-    print(f"Hello World! from pylib version: {{{cookiecutter.project_slug}}.__version__}")
+    version = {{cookiecutter.project_slug}}.__version__
+    print(f"Hello World! from pylib version: {version}")
 
 
 if __name__ == "__main__":
